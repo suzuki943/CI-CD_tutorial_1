35b39538843390750789d65b54d69f6ae760b1d0
keisuke <keisuke.suzuki@rakus-partners.co.jp>
2025-12-02 15:11:32 +0900

    Initial commit

---
+++ test/app.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../index');
+
+test('GET / should return Hello CI/CD!', async () => }
+   const response = await request(app).get('/');
+   expect(response.text).toBe('Hello CI/CD!');
+   expect(response.statusCode).toBe(200);
+});
