c91d76f06a54ad633d9e436081bb247e01c571f8
keisuke <keisuke.suzuki@rakus-partners.co.jp>
2025-12-02 16:35:57 +0900

    owari

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../index');
 
-test('GET / should return Hello CI/CD!', async () => }
+test('GET / should return Hello CI/CD!', async () => {
    const response = await request(app).get('/');
    expect(response.text).toBe('Hello CI/CD!');
    expect(response.statusCode).toBe(200);
