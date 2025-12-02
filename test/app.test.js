72e2991717bec237588756a5b12fbbf62ee7d991
keisuke <keisuke.suzuki@rakus-partners.co.jp>
2025-12-02 16:42:47 +0900

    owaranai

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,19 @@
 const request = require('supertest');
 const app = require('../index');
 
-test('GET / should return Hello CI/CD!', async () => {
-   const response = await request(app).get('/');
-   expect(response.text).toBe('Hello CI/CD!');
-   expect(response.statusCode).toBe(200);
+let server;
+
+beforeAll(() => {
+  server = app.listen(3000);
 });
+
+afterAll(done => {
+  server.close(done); // サーバーを閉じて Jest を終了させる
+});
+
+test('GET / should return Hello CI/CD!', async () => {
+  const response = await request(app).get('/');
+  expect(response.text).toBe('Hello CI/CD!');
+  expect(response.statusCode).toBe(200);
+});
+
